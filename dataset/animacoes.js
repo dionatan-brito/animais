c656a9b58b8d5790a9cf7c7a6b92dbab340755ea
dionatan-brito <94499334+dionatan-brito@users.noreply.github.com>
2022-09-21 15:30:41 -0300

    animais

---
+++ dataset/animacoes.js
@@ -0,0 +1,16 @@
+import initTabNav from "../js/modules/inittabnav.js";
+import initAccordion from "../js/modules/initaccordion.js";
+import {
+    initScrollSuave,
+    initAnimaScroll
+} from "../js/modules/scrollsuave.js";
+
+
+initTabNav();
+
+initAccordion();
+
+
+initScrollSuave();
+
+initAnimaScroll();
