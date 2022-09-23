a3053d9889a8ec98a91ac220dd58d3de3e186f93
dionatan-brito <94499334+dionatan-brito@users.noreply.github.com>
2022-09-23 13:18:27 -0300

    dropdownmenuatualizado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,4 +1,31 @@
+import { outSideclick } from "./outsideclick.js";
 export default function dropDownMenu(){
 
+    const dropDownMenus = document.querySelectorAll('[data-dropdown]');
+    
+    dropDownMenus.forEach(menu =>{
+        ['touchstart', 'click'].forEach(userEvent =>{
+        menu.addEventListener(userEvent, handleClick);
+    
+        })
+    })
+    
+    function handleClick(event){
+        event.preventDefault();
+        this.classList.add("active");
+        outSideclick(this,['touchstart', 'click'],()=>{
+            this.classList.remove("active");
+        })
+    }
+
 }
+
+
+
+
+
+
+
+
+
 
